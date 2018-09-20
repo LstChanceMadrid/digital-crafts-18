548669c0b9703cd689f6150466a7580ef5a1924e
LstChanceMadrid <michael.madrid@live.com>
2018-09-20 13:13:58 -0500

    prompts calculator to check for valid inputs

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
-first_number = float(input("Enter your first number: "))
-operand = input("Enter an operand (+, - , * , /): ")
-second_number = float(input("Enter your second number: "))
+#first_number = float(input("Enter your first number: "))
+#operand = input("Enter an operand (+, - , * , /): ")
+
+print("Hello, your calculator is ready!")
+print("You may press 'q' at anytime to quit.")
+
 
 def calculate(first_number, operand, second_number):
 
@@ -13,7 +16,12 @@ def calculate(first_number, operand, second_number):
     elif (operand == "/"):
         divide(first_number, second_number)
     else:
-        invalid(first_number, operand, second_number)
+        print("************")
+        print("Invalid operand, try again")
+        print("************")
+        return False
+
+            
 
 
 def add(first_number, second_number):
@@ -32,9 +40,30 @@ def divide(first_number, second_number):
     result = first_number / second_number
     print(result)
 
-def invalid(first_number, operand, second_number):
-    print("Invalid input, try again.")
+while True:
+    try:
+        first_number = input("Enter your first number: ")
+        if (first_number.lower() == 'q'):
+            break
+        first_number_t = float(first_number)
+        operand = input("Enter an operand (+, - , * , /): ")
+        if (operand == 'q'):
+            break  
+        second_number = input("Enter your second number: ")
+        if (second_number == 'q'):
+            break
+        second_number_t = float(second_number)
+        calculate(first_number_t, operand, second_number_t)          
 
-calculate(first_number, operand, second_number)
+        
+    except:
+
+        if ValueError:
+            print("************")
+            print("Invalid number, try again")
+            print("************")
+
+
+
 
 
